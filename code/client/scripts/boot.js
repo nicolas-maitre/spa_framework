3fef393e6ae65397ecde3c3268a9c3244bd2f224
MAITRE Nicolas <Nicolas.MAITRE@cpnv.ch>
2019-09-27 13:32:46 +0200

    created dynamic view loader

--- code/client/scripts/boot.js
+++ code/client/scripts/boot.js
@@ -1,4 +1,11 @@
+var globalMemory = {};
+
 //instanciate managers
 var pagesManager = new PagesManager();
 var builder = new Builder();
+
+//init
+pagesManager.preloadViews();
+pagesManager.changePage("home");
+
 console.log("init completed");
