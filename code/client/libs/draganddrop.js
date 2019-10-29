ecabb610afa370a105df9c433fd3a8b9978250d1
Florian Bergmann <florian.bergmann@cpnv.ch>
2019-10-29 10:19:56 +0100

    resolve draganddrop issues

--- code/client/libs/draganddrop.js
+++ code/client/libs/draganddrop.js
@@ -1,3 +1,4 @@
+"use strict"
 /**
  * Class DragAndDrop
  * Author : Bergmann Florian
@@ -8,28 +9,38 @@
  * - function addElemBefore
  * - function addElemAfter
  */
-
-"use strict"
 var DragAndDrop = function(){
     var _this = this;
     var draggedElem;
-
+    /**
+     * Methode to build drag and drop
+     * @param classListToDrop define elements where to drop moving element
+     * @param classElementToDrag define elements are possible to move
+     */
     this.buildDragAndDrop = function(classListToDrop, classElementToDrag){
-        this.addDrop(classListToDrop);
-        this.addDrag(classElementToDrag);
+        _this.addDrop(classListToDrop);
+        _this.addDrag(classElementToDrag);
     }
+    /**
+     * Methode to add possibility to drop
+     * @param classToDrop define elements where to drop moving element 
+     */
     this.addDrop = function(classToDrop){
         //add event dragover and drop on all list to move elements between us
         Array.prototype.forEach.call(document.getElementsByClassName(classToDrop), function(elem){
             elem.classList.add("toDrop");
             elem.addEventListener("dragover", function(event){
-                this.moveElem(this.draggedElem, event.target);
+                _this.moveElem(draggedElem, event.target);
             }); 
             elem.addEventListener("drop", function(event){
-                this.moveElem(this.draggedElem, event.target);
+                _this.moveElem(draggedElem, event.target);
             });
         });
     }
+    /**
+     * Methode to add possibility to move
+     * @param classToDrag define elements are possible to move
+     */
     this.addDrag = function(classToDrag){
         //add event dragstart to allow on element to drag and display where is dropped and add event to remove the effect
         Array.prototype.forEach.call(document.getElementsByClassName(classToDrag), function(elem){
@@ -37,15 +48,18 @@ var DragAndDrop = function(){
             elem.addEventListener("dragstart", function(event){
                 event.dataTransfer.setData("0", 0);
                 if(event.target.className.includes("toDrag")){
-                    this.draggedElem = event.target;
-                    this.draggedElem.classList.add("hovered");
+                    draggedElem = event.target;
+                    draggedElem.classList.add("hovered");
                 }
             });
             elem.addEventListener("dragend", function(event){
-                this.draggedElem.classList.remove("hovered");
+                draggedElem.classList.remove("hovered");
             });
         });
     }
+    /**
+     * Methode to remove possibility to drop
+     */
     this.removeDrop = function(){
         Array.prototype.forEach.call(document.getElementsByClassName("toDrop"), function(elem){
             element.removeEventListener("dragover");
@@ -53,23 +67,31 @@ var DragAndDrop = function(){
             elem.classList.remove("toDrop");
         });
     }
+    /**
+     * Methode to remove possibility to move
+     */
     this.removeDrag = function(){
         Array.prototype.forEach.call(document.getElementsByClassName("toDrag"), function(elem){
             element.removeEventListener("dragstart");
             elem.classList.remove("toDrag");
         });
     }
-    //function to move element between list
-
+    /**
+     * Methode to move an element
+     * @param elemToDrag elem to move
+     * @param elementToDrop elem where we pointing the elem to drop
+     */
     this.moveElem = function(elemToDrag, elementToDrop){
-        while(!elementToDrop.parentNode.className.includes("toDrop")){
+        //move in parent DOM while the class granted to drop
+        while(!elementToDrop.parentNode.className.includes("toDrop") && !elementToDrop.className.includes("toDrop")){
             elementToDrop = elementToDrop.parentNode;
+            if(elementToDrop == document.body)return;
         }
         if(elementToDrop.className.includes("toDrag")){
             if(elementToDrop != elemToDrag){
                 if(elementToDrop.checkMouseIsTop())
-                    addElemBefore(elemToDrag, elementToDrop);
-            else elemToDrag.moveAfter(elementToDrop)
+                    elemToDrag.addElemBefore(elementToDrop);
+                else elemToDrag.addElemAfter(elementToDrop)
             }
         }else if(elementToDrop.className.includes("toDrop")){
             elementToDrop.appendChild(elemToDrag);
