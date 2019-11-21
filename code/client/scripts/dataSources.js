c16ef1907522ca60025a98df9fac64aa9440ff64
Florian Bergmann <florian.bergmann@cpnv.ch>
2019-11-21 15:22:41 +0100

    add few call api method

--- code/client/scripts/dataSources.js
+++ code/client/scripts/dataSources.js
@@ -1,11 +1,70 @@
 "use strict";
 function DataSources(){
+	//get
+	/**
+	 * get all quizz availible
+	 */
 	this.allAvailibleQuizzes = async function(){
 		var res = await apiManager.call("quizzes");
 		return res.ok ? res.data : [];//return only if data
 	};
+	/**
+	 * get all quizzes
+	 */
 	this.allQuizzes = async function(){
 		var res = await apiManager.call("quizzes");
 		return res.ok ? res.data : [];//return only if data
 	};
+	/**
+	 * get 1 quizz
+	 */
+	this.quizz = async function(id){
+		var res = await apiManager.call("quizzes/"+id);
+		return res.ok ? res.data : [];//return only if data
+	}
+	//post
+	/**
+	 * create quizz
+	 * @param {object{name: "", description: ""}} data 
+	 */
+	this.addQuizz = async function(data){
+		var options = {
+			method: "POST",
+			bodyParams: data
+		}
+		var res = await apiManager.call("quizzes", options);
+		return res.ok ? res.data : [];//return only if data
+	}
+	/**
+	 * 
+	 * @param {int} id id of quizz to add questions
+	 * @param {object{title: "", type:""}} data
+	 */
+	this.addQuestion = async function(id, data){
+		var options = {
+			method: "POST",
+			bodyParams: data
+		}
+		var res = await apiManager.call("questions/"+id, options);
+		return res.ok ? res.data : [];//return only if data
+	}
+	//put
+	this.updateQuizz = async function(id, data){
+		var options = {
+			method: "PUT",
+			bodyParams: data
+		}
+		var res = await apiManager.call("quizzes/"+id, options);
+		return res.ok ? res.data : [];//return only if data
+	}
+	this.updateQuestion = async function(id, data){
+		var options = {
+			method: "PUT",
+			bodyParams: data
+		}
+		var res = await apiManager.call("questions/"+id, options);
+		return res.ok ? res.data : [];//return only if data
+	}
+	//delete
+
 }
